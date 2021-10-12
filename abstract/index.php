15fade480c63402a517f31516ac815256de1596f
qiuxan <qiuxan@gmail.com>
2021-10-13 06:04:59 +1100

    use of interface and implements

---
+++ abstract/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xianqiu
+ * Date: 12/10/21
+ * Time: 10:37 PM
+ */
---
+++ handshakesAndInterfaces/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xianqiu
+ * Date: 13/10/21
+ * Time: 5:39 AM
+ */
